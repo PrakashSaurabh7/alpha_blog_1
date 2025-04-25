96878aa5c6b9c290f4fe471ada0a31a71241a53a
PrakashSaurabh7 <saurabhprakash7777@gmail.com>
2025-04-26 01:34:06 +0530

    after configuring flash messages

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,19 +6,28 @@ class ArticlesController < ApplicationController
     @article = TabArticle.find(params[:id])
   end
   def new
+    @article = TabArticle.new
   end
   def add
     @article = TabArticle.new(params.require(:tab_article).permit(:tab_title, :tab_description))
-    @article.save
-    redirect_to "/"
+    if @article.save
+      flash[:notice] = "Article created successfully"
+      redirect_to "/"
+    else
+      render "new"
+    end
   end
   def edit
     @article = TabArticle.find(params[:id])
   end
   def update
     @article = TabArticle.find(params[:id])
-    @article.update(params.require(:tab_article).permit(:tab_title, :tab_description))
-    redirect_to "/"
+    if @article.update(params.require(:tab_article).permit(:tab_title, :tab_description))
+      flash[:notice] = "Data updated successfully"
+      redirect_to "/"
+    else
+      render "edit"
+    end
   end
   def delete
     @article = TabArticle.find(params[:id])
